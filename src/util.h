b5718de9aad5cdf6280ed498f96731095377eb12
Neal H. Walfield <neal@gnu.org>
2011-06-05 17:37:28 +0200

    Make 'make dist' work and make the generated tarball compile.

--- src/util.h
+++ src/util.h
@@ -1,3 +1,20 @@
+/* util.h - Utility functions.
+   Copyright (C) 2011 Neal H. Walfield <neal@walfield.org>
+
+   Smart storage is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 3, or (at
+   your option) any later version.
+
+   Smart storage is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
+
 #include <inttypes.h>
 #include <stdint.h>
 #include <sys/time.h>
@@ -141,6 +158,8 @@ sqlite3_exec_printf (sqlite3 *db, const char *sql,
   va_start (ap, errmsg);
 
   char *s = sqlite3_vmprintf (sql, ap);
+
+  // fprintf (stderr, "%s\n", s);
 
   int ret = sqlite3_exec (db, s, callback, cookie, errmsg);
 
